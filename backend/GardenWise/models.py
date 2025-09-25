3c6208596e4eab44b3edf7ad2f011232ff9007d8
Olivia Sabb <sabb.olivia@hotmail.com>
2025-09-25 16:36:05 -0500

    - Initialize Django project and app - Create a new app with `python manage.py startapp` - Add the new app to `INSTALLED_APPS` in settings.py - Create basic models, views, and URLs

---
+++ backend/GardenWise/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+# This is where we'll define data models, Create your models here.
---
+++ backend/GardenWise/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+#This is where we'll define the logic for handling HTPP requests and returning responses. 
+#Create your views here.
