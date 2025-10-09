5f14e676b72ab42f86bddd98324a39c9520e31d2
MichaelWHamilton <mwhamilton@ualr.edu>
2025-10-09 02:14:04 -0500

    Add navigation for Home, Garden Planner, Plant Information, Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,32 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import GardenPlanner from './pages/GardenPlanner';
+import PlantInfo from './pages/PlantInfo';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <nav style={{ display: "flex", gap: "1rem" }}>
+    <nav>
+      <div className="nav-left">
         <Link className="btn" to="/">Home</Link>
         <Link className="btn" to="/garden-planner">Garden Planner</Link>
-      </nav>
+        <Link className="btn" to="/plant-info">Plant Info</Link>
+      </div>
+      
+      <div className="nav-spacer" />
+
+      <div className="nav-right">
+        <Link className="btn login-btn" to="/login">Login</Link>
+      </div>
+    </nav>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/garden-planner" element={<GardenPlanner/>}/>
+        <Route path="/plant-info" element={<PlantInfo/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>    
   );
---
+++ frontend/src/pages/PlantInfo.jsx
@@ -0,0 +1,6 @@
+function PlantInfo(){
+    return <h1>Plant Information</h1>;
+    
+}
+
+export default PlantInfo;
