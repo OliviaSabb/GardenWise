75269e79edde051997a75c1c7852ddf7d5d00962
Olivia Sabb <sabb.olivia@hotmail.com>
2025-10-10 16:19:07 -0500

    Update settings.py and clean up ignored files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend',
-    'GardenWise.apps.GardenwiseConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +54,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    "*"
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     ]
